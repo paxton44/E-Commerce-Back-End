7b25e746d0a825d462cd09d94c29d88341d1290b
Mitchell Flood <mitchflood1@gmail.com>
2021-05-31 13:51:11 -0600

    adding index.js to models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//Activity 26, 28 index.js each have a good example of how to lay out belongsTO, belongTOMany tags, Tags belongToMany Products
+
 // Products belongsTo Category
+Products.belongsTo(Category);
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag',
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Product, {
+  foreignKey: 'product_id',
+});
+
 
 module.exports = {
   Product,
