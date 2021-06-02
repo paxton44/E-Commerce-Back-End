a3d6a1d726a5fd2b38ba13b6df6098c45f072811
Mitchell Flood <mitchflood1@gmail.com>
2021-06-02 13:31:42 -0600

    seeded db

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({force: false}).then(() => {
+
+app.listen(PORT, () => console.log("App listening on port"));
 });
+
 
 //heres the fix to the error im getting from sequelize https://stackoverflow.com/questions/61301940/sequelize-hasmany-is-working-fine-but-the-inverse-relation-is-not-working/61304209#61304209
